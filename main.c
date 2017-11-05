79f4d61fdbc57e78011d41217150718ffd0c23ec
Tarek <tarekabderaouf.belkadi.dz@viacesi.fr>
2017-11-05 20:33:02 +0100

    compute addition of two integers

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int addition (int a,int b)
 {
-    printf("Hello world!\n");
+   return a+b;
+}
+
+int main ()
+{
+
+    printf("%d\n", addition(1,4));
     return 0;
 }
